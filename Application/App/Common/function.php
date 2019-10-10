01be83cd25b1da57a3fa994f6878f5879a3dd97b
cui <cuiruijunphp@163.com>
2019-10-10 11:22:54 +0800

    计算两个点之间的坐标

--- Application/App/Common/function.php
+++ Application/App/Common/function.php
@@ -40,3 +40,24 @@ function random_from_dev($length)
 
 	return substr($result, 0, $length);
 }
+
+/**
+ * 根据两点间的经纬度计算距离(单位:米)
+ * @param $lng1
+ * @param $lat1
+ * @param $lng2
+ * @param $lat2
+ * @return int
+ */
+function getDistance($lng1, $lat1, $lng2, $lat2)
+{
+	//将角度转为狐度
+	$radLat1 = deg2rad($lat1);//deg2rad()函数将角度转换为弧度
+	$radLat2 = deg2rad($lat2);
+	$radLng1 = deg2rad($lng1);
+	$radLng2 = deg2rad($lng2);
+	$a = $radLat1 - $radLat2;
+	$b = $radLng1 - $radLng2;
+	$s = 2 * asin(sqrt(pow(sin($a / 2), 2) + cos($radLat1) * cos($radLat2) * pow(sin($b / 2), 2))) * 6378.137 * 1000;
+	return $s;
+}
