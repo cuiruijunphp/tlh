e57c6192ec18dc30c68f29179a69a8119c9f8546
cui <cuiruijunphp@163.com>
2019-10-24 11:32:27 +0800

    配置UPLOAD_URL

--- Application/App/Conf/config.php
+++ Application/App/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //define('UPLOAD_URL', '/Uploads/');
-define('UPLOAD_URL', $_SERVER['SERVER_NAME'] . '/Uploads/');
+define('UPLOAD_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/Uploads/');
 
 return array(
 	//'配置项'=>'配置值'
