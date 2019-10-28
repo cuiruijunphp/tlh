fdd6a7e0ddef28be3d715400fdc855e3ce4b491b
cui <cuiruijunphp@163.com>
2019-10-28 11:12:42 +0800

    管理后台配置

--- Application/Manage/Conf/config.php
+++ Application/Manage/Conf/config.php
@@ -1,8 +1,22 @@
 <?php
 
-//define('UPLOAD_URL', '/Uploads/');
-define('UPLOAD_URL', $_SERVER['SERVER_NAME'] . '/Uploads/');
-
 return array(
 	//'配置项'=>'配置值'
+	//'配置项'=>'配置值'
+	//默认动作和控制器的设置
+	'DEFAULT_MODULE'        =>  'Manage',  // 默认模块
+	'DEFAULT_CONTROLLER'    =>  'User', // 默认控制器名称
+	'DEFAULT_ACTION'        =>  'index', // 默认操作名称
+
+	//页面trace功能
+	'SHOW_PAGE_TRACE'       =>   false,
+
+	//开启路由模式
+	'URL_ROUTER_ON'   => true,
+
+	//开启url模式
+//	'URL_MODEL'             =>   0,
+
+	//url大小写敏感设置
+//	'URL_CASE_INSENSITIVE'  =>  true,   // 默认false 表示URL区分大小写 true则表示不区分大小写
 );
