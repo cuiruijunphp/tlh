dc87a23ec42721b1217ab6841d90d4a27a90e6bf
cui <cuiruijunphp@163.com>
2019-10-28 10:32:02 +0800

    管理后台 - 技能类型管理

--- Application/Manage/Controller/SkillTypeController.class.php
+++ Application/Manage/Controller/SkillTypeController.class.php
@@ -4,6 +4,76 @@ use Think\Controller;
 class SkillTypeController extends BaseController {
 
     public function index(){
+
+		$skill_type_model = D('SkillType');
+
+		$page = I('get.p');
+		$skill_type_list = $skill_type_model->get_page_list(null, $page);
+		$skill_type_count = $skill_type_model->get_count();
+		$data['list'] = $skill_type_list;
+		// 加上分页
+		$data['page'] = $this->page_new($skill_type_count);
+		$this->assign($data);
 		$this->display();
     }
+
+    public function edit(){
+		$skill_type_model = D('SkillType');
+
+		if (IS_POST) {
+			$params = I('post.');
+
+			if (!$params['type_name']) {
+				$this->result_return(null, 500, '技能类型名称不能为空');
+			}
+
+			$insert_data = [
+				'type_name' => $params['type_name'],
+				'is_show' => (int)$params['is_show'],
+			];
+
+			if(!$params['id']){
+				//新增
+				$result = $skill_type_model->insert_one($insert_data);
+
+				if($result){
+					$this->result_return(['result' => 1]);
+				}else{
+					$this->result_return(null, 500, '添加技能类型失败');
+				}
+			}else{
+				//修改
+				$update_result = $skill_type_model->update_data(['id' => $params['id']], $insert_data);
+				if($update_result !== false){
+					$this->result_return(['result' => 1]);
+				}else{
+					$this->result_return(null, 500, '修改技能类型失败');
+				}
+			}
+		}
+
+		//参数
+		if (!empty(I('get.id'))){
+			$skill_type_list = $skill_type_model -> get_one(['id' => I('get.id')]);
+		}
+
+		$data['list'] = $skill_type_list;
+		$this->assign($data);
+		$this->display();
+	}
+
+	public function change_status()
+	{
+		$skill_type_model = D('SkillType');
+
+		//判断当前传的参数和数据库中是否相同,如果相同则报错
+		$where['id'] = I('post.id');
+		$data['is_show'] = I('post.is_show');
+		$result = $skill_type_model->update_data($where, $data);
+		if ($result) {
+			$this->result_return(['result' => 1]);
+		} else {
+			$this->result_return(null, 500, '修改失败,请重试');
+		}
+	}
 }
