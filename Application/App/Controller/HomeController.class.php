e574ef1860a66b819fd3070a7a9d0149448f26a2
cui <cuiruijunphp@163.com>
2019-10-29 18:36:04 +0800

    上报位置接口

--- Application/App/Controller/HomeController.class.php
+++ Application/App/Controller/HomeController.class.php
@@ -27,4 +27,92 @@ class HomeController extends BaseController
 		}
 		$this->result_return($banner_list);
 	}
+
+
+	/**
+	 * 方法说明
+	 * @author cuirj
+	 * @date   2019/10/29 下午4:08
+	 * @url    /app/home/get_list/
+	 * @method get
+	 *
+	 * @param  int param
+	 * @return  array
+	 */
+	public function get_list(){
+		//分不同情况
+		//默认根据发布时间倒序
+		//根据距离优先
+		//根据人气最高-预约人数
+		//根据分类
+		//根据技能类型
+
+		$params = I('get.');
+
+		$type_id = $params['type_id'];
+		$type_id = $params['type_id'];
+		$type_id = $params['type_id'];
+
+		$demand_model = D('UserDemand');
+		$skill_model = D('UserSkill');
+	}
+
+	/**
+	 * 方法说明
+	 * @author cuirj
+	 * @date   2019/10/29 下午6:12
+	 * @url    app/home/get_user_list_by_type_id/
+	 * @method get
+	 *
+	 * @param  int param
+	 * @return  array
+	 */
+	public function get_user_list_by_type_id(){
+		// 根据分类
+		//根据年龄
+		//根据性别筛选
+		//根据技能类型
+
+		$params = I('get.');
+
+		$demand_model = D('UserDemand');
+		$skill_model = D('UserSkill');
+	}
+
+	/**
+	 * 上传当前用户的当前位置
+	 * @author cuirj
+	 * @date   2019/10/29 下午6:24
+	 * @url    app/home/submit_location
+	 * @method get
+	 *
+	 * @param  int param
+	 *             return  array
+	 */
+	public function submit_location(){
+
+		$get_param = file_get_contents('php://input');
+		$params = json_decode($get_param, true);
+
+		$user_id = $this->user_id;
+		$lat = $params['latitude'];
+		$long = $params['longitude'];
+
+		$data = [
+			'latitude' => $lat,
+			'longitude' => $long,
+			'user_id' => $user_id
+		];
+		$location_model = D('UserLocation');
+		$location_info = $location_model->get_one(['user_id' => $user_id]);
+		if($location_info){
+			//更新
+			$result = $location_model->update_data(['id' => $location_info['id']], $data);
+		}else{
+			// 新增
+			$result = $location_model->insert_one($data);
+		}
+
+		$this->result_return(['result' => 1]);
+	}
 }
