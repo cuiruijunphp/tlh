02748c02c5e49632775fc70c13782d945ea20d60
cui <cuiruijunphp@163.com>
2019-10-30 21:09:32 +0800

    增加id

--- Application/App/Model/UserSkillModel.class.php
+++ Application/App/Model/UserSkillModel.class.php
@@ -15,11 +15,11 @@ class UserSkillModel extends CommonModel{
 			$type_id_where = ' = ' . $type_id;
 		}
 
-		$sql = '(select skill_name as title,user_id,is_online,head_img,user_name,type_id,s.add_time,longitude,latitude,img,"skill" as type from user_skill s left join users u on s.user_id = u.id where s.status =1 and s.type_id '. $type_id_where . ')';
+		$sql = '(select s.id,skill_name as title,user_id,is_online,head_img,user_name,type_id,s.add_time,longitude,latitude,img,"skill" as type from user_skill s left join users u on s.user_id = u.id where s.status =1 and s.type_id '. $type_id_where . ')';
 
 		$sql .= ' union ';
 
-		$sql .= '(select title,user_id,is_online,head_img,user_name,type_id,d.add_time,longitude,latitude,null as img,"demand" as type from user_demand as d left join users u on d.user_id = u.id  where status=1  and end_time > '.time().' and type_id ' . $type_id_where . ')';
+		$sql .= '(select d.id,title,user_id,is_online,head_img,user_name,type_id,d.add_time,longitude,latitude,null as img,"demand" as type from user_demand as d left join users u on d.user_id = u.id  where status=1  and end_time > '.time().' and type_id ' . $type_id_where . ')';
 
 		$sql .= ' limit ' . $offset . ',' . $page_size;
 
@@ -47,11 +47,11 @@ class UserSkillModel extends CommonModel{
       )
     ) *1000 as distance from ';
 
-		$sql .= '((select skill_name as title,user_id,is_online,head_img,user_name,type_id,s.add_time,longitude,latitude,img,"skill" as type from user_skill s left join users u on s.user_id = u.id where s.status =1)';
+		$sql .= '((select s.id,skill_name as title,user_id,is_online,head_img,user_name,type_id,s.add_time,longitude,latitude,img,"skill" as type from user_skill s left join users u on s.user_id = u.id where s.status =1)';
 
 		$sql .= ' union ';
 
-		$sql .= '(select title,user_id,is_online,head_img,user_name,type_id,d.add_time,longitude,latitude,null as img,"demand" as type from user_demand as d left join users u on d.user_id = u.id  where status=1  and end_time > '.time(). ')) as tmp';
+		$sql .= '(select d.id,title,user_id,is_online,head_img,user_name,type_id,d.add_time,longitude,latitude,null as img,"demand" as type from user_demand as d left join users u on d.user_id = u.id  where status=1  and end_time > '.time(). ')) as tmp';
 
 		$sql .= ' order by distance limit ' . $offset . ',' . $page_size;
 
@@ -63,11 +63,11 @@ class UserSkillModel extends CommonModel{
 	 */
 	public function get_skill_demand_by_view($offset = 0, $page_size=10){
 
-		$sql = '(select skill_name as title,user_id,is_online,head_img,user_name,type_id,s.add_time,longitude,latitude,img,"skill" as type, reservation_count as r_count from user_skill s left join users u on s.user_id = u.id where s.status =1)';
+		$sql = '(select s.id,skill_name as title,user_id,is_online,head_img,user_name,type_id,s.add_time,longitude,latitude,img,"skill" as type, reservation_count as r_count from user_skill s left join users u on s.user_id = u.id where s.status =1)';
 
 		$sql .= ' union ';
 
-		$sql .= '(select title,user_id,is_online,head_img,user_name,type_id,d.add_time,longitude,latitude,null as img,"demand" as type,(LENGTH(`applicants`) - LENGTH(REPLACE(`applicants`,",", "")))  as r_count from user_demand as d left join users u on d.user_id = u.id  where status=1  and end_time > '.time().')';
+		$sql .= '(select d.id,title,user_id,is_online,head_img,user_name,type_id,d.add_time,longitude,latitude,null as img,"demand" as type,(LENGTH(`applicants`) - LENGTH(REPLACE(`applicants`,",", "")))  as r_count from user_demand as d left join users u on d.user_id = u.id  where status=1  and end_time > '.time().')';
 
 		$sql .= 'order by r_count desc limit ' . $offset . ',' . $page_size;
 
