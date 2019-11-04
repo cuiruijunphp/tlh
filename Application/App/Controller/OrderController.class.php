94645ea228f61c61c9933e5ea5e28918f6bf7b95
cui <cuiruijunphp@163.com>
2019-11-04 22:56:20 +0800

    支付

---
+++ Application/App/Controller/OrderController.class.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Controller;
+use Think\Controller;
+class OrderController extends BaseController {
+
+	/**
+	 * 创建订单
+	 * @date   2019/11/4 下午4:43
+	 * @url    app/order/create_order/
+	 * @method post
+	 *
+	 * @param  int page
+	 * @param  int page_size
+	 * @return  array
+	 */
+    public function create_order(){
+		$get_param = file_get_contents('php://input');
+		$params = json_decode($get_param, true);
+
+		//1-购买会员,2-发布需求,3-预约技能
+		$source_type = $params['source_type'];
+		$source_id = $params['source_id'];
+		$vip_aging_type = $params['vip_aging_type'];//vip时效
+
+		$price = $params['price'];
+		$pay_type = $params['pay_type'];
+
+		// 如果是vip时效
+		if($source_type == 1){
+			$source_type_arr = [
+				1 => [
+					'price' => 10,
+					'time' => 30,
+				],
+				2 => [
+					'price' => 100,
+					'time' => 365,
+				],
+			];
+
+			if($source_type_arr[$vip_aging_type]['price'] != $price){
+				$this->result_return(null, 500, '创建订单失败,金额非法');
+			}
+		}elseif($source_type == 2){
+			//验证是否存在该条需求
+		}elseif($source_type == 3){
+			//验证是否存在该条需技能
+		}
+
+		$user_id = $this->user_id;
+
+    	//创建订单号
+		$order_id = session_create_random_id(32);
+
+		$insert_data = [
+			'order_id' => $order_id,
+			'user_id' => $user_id,
+			'price' => $price,
+			'pay_type' => $pay_type,
+			'source_type' => $source_type,
+			'source_id' => $source_id,
+		];
+
+		if($source_type == 1){
+			$insert_data['extra_info'] = json_encode(['vip_aging_type' => $vip_aging_type]);
+		}
+
+		$order_model = D('Order');
+		$insert_result = $order_model->insert_one($insert_data);
+
+		if($insert_result === false){
+			$this->result_return(null, 500, '创建订单失败');
+		}
+
+		$this->result_return(['order_id' => $insert_result]);
+	}
+}
---
+++ Application/App/Model/OrderModel.class.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Model;
+use Think\Model;
+
+class OrderModel extends CommonModel{
+
+}
