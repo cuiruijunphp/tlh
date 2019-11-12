76debfaffcf995c2b3e347572d16f4f0eabac2d9
cui <cuiruijunphp@163.com>
2019-11-12 14:59:48 +0800

    支付报认证账户真实姓名

--- Application/App/Controller/PersonalController.class.php
+++ Application/App/Controller/PersonalController.class.php
@@ -191,10 +191,14 @@ class PersonalController extends BaseController {
 		$weixin_account = $params['weixin_account'];
 		$weibo_account = $params['weibo_account'];
 		$alipay_account = $params['alipay_account'];
+		$alipay_real_name = $params['alipay_real_name'];
 
+		if(!$weixin_account || !$weibo_account || !$alipay_account){
+			$this->result_return(null, 500, '请绑定认证账户哦');
+		}
 		$user_model = D('Users');
 
-		$update_result = $user_model->update_data(['id' => $this->user_id], ['weixin_account' => $weixin_account, 'weibo_account' => $weibo_account, 'alipay_account' => $alipay_account, 'is_vefify' => 1]);
+		$update_result = $user_model->update_data(['id' => $this->user_id], ['weixin_account' => $weixin_account, 'weibo_account' => $weibo_account, 'alipay_account' => $alipay_account, 'is_vefify' => 1, 'alipay_real_name' => $alipay_real_name]);
 
 		if($update_result === false){
 			$this->result_return(null, 500, '绑定账号失败');
