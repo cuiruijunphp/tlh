b70f34bef8287dcab7bf33508ae85aa28ca1c9fe
cui <cuiruijunphp@163.com>
2019-12-20 17:49:17 +0800

    筛选条件组合

--- Application/App/Controller/HomeController.class.php
+++ Application/App/Controller/HomeController.class.php
@@ -211,4 +211,104 @@ class HomeController extends BaseController
 
 		$this->result_return($notice_info);
 	}
+
+	/**
+	 * 方法说明
+	 * @author cuirj
+	 * @date   2019/10/29 下午4:08
+	 * @url    /app/home/get_list/
+	 * @method get
+	 *
+	 * @param  int param
+	 * @return  array
+	 */
+	public function get_list_new(){
+		//分不同情况
+		//默认根据发布时间倒序
+		//根据距离优先
+		//根据人气最高-预约人数
+		//根据分类
+		//根据技能类型
+
+		$params = I('get.');
+
+		$type_id = $params['type_id'];
+		$type = $params['type'] ? $params['type'] : 'all';
+		$latitude = $params['latitude'];
+		$longitude = $params['longitude'];
+		$keyword = $params['keyword'];
+		$page = $params['page'] ? $params['page'] : 1;
+		$page_size = $params['page_size'] ? $params['page_size'] : 5;
+
+		$offset = ($page - 1) * $page_size;
+
+		$uid = $this->user_id;
+
+		$skill_model = D('UserSkill');
+		$skill_type_model = D('SkillType');
+
+		if($type_id){
+			//如果是一级分类,则把下面所有的二级分类都取出来
+			$skill_type_info =$skill_type_model->get_one(['id' => $type_id]);
+			if(!$skill_type_info){
+				$this->result_return(null, 1, '请传入正确的类型');
+			}
+
+			if($skill_type_info['parent_id'] == 0){
+				$skill_type_list = $skill_type_model->get_list(['parent_id' => $type_id]);
+
+				$skill_type_ids = array_column($skill_type_list, 'id');
+				$type_ids = implode(',', $skill_type_ids) . ',' . $type_id;
+			}else{
+				$type_ids = $type_id;
+			}
+		}
+
+		switch ($type){
+			case 'hot':
+				// 如果是按热度排序,则将此字段置空为1
+				$is_hot = 1;
+			break;
+
+			case 'demand':
+				$is_demand = 1;
+			break;
+
+			case 'skill':
+				$is_skill = 1;
+			break;
+
+			case 'keyword':
+				if(!$keyword){
+					$this->result_return(null, 1, '请传入要查询的内容');
+				}
+			break;
+
+			case 'distance':
+				$is_distance = 1;
+			break;
+
+			default:
+				break;
+
+		}
+
+		$result = $skill_model->get_common_list($uid, $type_ids, $latitude, $longitude, $is_distance, $is_hot, $is_demand, $is_skill, $keyword, $offset, $page_size);
+
+		// 对结果进行处理
+		if($result){
+			foreach($result as $k => $v){
+				if($type != 'distance'){
+					// 如果不是根据距离排序 查到的结果,则用php计算距离
+					$result[$k]['distance'] = get_distance($v['longitude'], $v['latitude'], $longitude, $latitude);
+				}
+
+				//头像加上地址前缀
+				$result[$k]['img'] = $v['img'] ? UPLOAD_URL . $v['img'] : '';
+				$result[$k]['head_img'] = UPLOAD_URL . $v['head_img'];
+			}
+		}
+
+		$this->result_return($result);
+	}
 }
--- Application/App/Model/UserSkillModel.class.php
+++ Application/App/Model/UserSkillModel.class.php
@@ -98,7 +98,7 @@ class UserSkillModel extends CommonModel{
 
 		$sql = '(select s.id,skill_name as title,user_id,is_online,head_img,user_name,type_id,s.add_time,longitude,latitude,img,"skill" as type, reservation_count as r_count from user_skill s left join users u on s.user_id = u.id where s.user_id != ' . $uid . ' and s.status =1)';
 
-		$sql .= 'order by add_time desc limit ' . $offset . ',' . $page_size;
+		$sql .= ' order by add_time desc limit ' . $offset . ',' . $page_size;
 
 		return $this->query($sql);
 	}
@@ -110,7 +110,7 @@ class UserSkillModel extends CommonModel{
 
 		$sql = '(select d.id,title,user_id,is_online,head_img,user_name,type_id,d.add_time,longitude,latitude,null as img,"demand" as type from user_demand as d left join users u on d.user_id = u.id  where d.user_id != ' . $uid . ' and status=1  and end_time > ' . time(). ')';
 
-		$sql .= 'order by add_time desc limit ' . $offset . ',' . $page_size;
+		$sql .= ' order by add_time desc limit ' . $offset . ',' . $page_size;
 
 		return $this->query($sql);
 	}
@@ -147,6 +147,109 @@ class UserSkillModel extends CommonModel{
 		$sql .= '(select d.id,title,user_id,is_online,head_img,user_name,type_id,d.add_time,longitude,latitude,null as img,"demand" as type from user_demand as d left join users u on d.user_id = u.id  where d.user_id != ' . $uid . ' and status=1  and end_time > '.time().' and d.title like "%' . $keyword . '%")';
 
 		$sql .= ' order by add_time desc limit ' . $offset . ',' . $page_size;
+
+		return $this->query($sql);
+	}
+
+	/*
+	 * 首页列表公共方法抽取
+	 * $uid 当前登录的uid
+	 * $type_id 技能类型id,逗号分隔的
+	 * $lat/$lng 当前登录用户的经纬度
+	 * $keyword 查询关键词
+	 * $is_hot 是否按热度排序
+	 * $is_distance 按距离排序
+	 * $is_demand 只取需求
+	 * $is_skill 只取技能
+	 * $offset 偏移量
+	 * $page_size 每页多少条
+	 */
+	public function get_common_list($uid, $type_id, $lat, $lng, $is_distance, $is_hot, $is_demand, $is_skill, $keyword, $offset = 0, $page_size=10){
+
+		$s_where = ' and s.user_id != ' . $uid;
+		$d_where = ' and d.user_id != ' . $uid;
+		// 如果有type_id
+		if($type_id){
+			if(count(explode(',', $type_id)) > 1){
+				$type_id = trim($type_id, ',');
+				$s_where .= ' and s.type_id in (' . $type_id . ')';
+				$d_where .= ' and d.type_id in (' . $type_id . ')';
+			}else{
+				$s_where .= ' and s.type_id = ' . $type_id;
+				$d_where .= ' and d.type_id = ' . $type_id;
+			}
+		}
+
+		//关键词
+		if($keyword){
+			$s_where = ' and s.skill_name like "%'. $keyword . '%"';
+			$d_where .= ' and d.title  like "%'. $keyword . '%"';
+		}
+
+		// 公用的查询字段和排序方式
+		//技能表获取的字段
+		$s_field = ' s.id,skill_name as title,user_id,is_online,head_img,user_name,type_id,s.add_time,longitude,latitude,img,"skill" as type';
+
+		$d_field = ' d.id,title,user_id,is_online,head_img,user_name,type_id,d.add_time,longitude,latitude,null as img,"demand" as type';
+
+		$order = ' add_time desc';
+
+		// 如果按热度排序
+		if($is_hot){
+			// 技能表获取的字段,多了个访问总数字段
+			$s_field = ' s.id,skill_name as title,user_id,is_online,head_img,user_name,type_id,s.add_time,longitude,latitude,img,"skill" as type, reservation_count as r_count';
+
+			// 需求表字段,多了个访问总数字段
+			$d_field = ' d.id,title,user_id,is_online,head_img,user_name,type_id,d.add_time,longitude,latitude,null as img,"demand" as type,(LENGTH(`applicants`) - LENGTH(REPLACE(`applicants`,",", "")))  as r_count';
+
+			$order = ' r_count desc';
+		}
+
+		// 按距离排序
+		if($is_distance){
+
+			// 如果是按照距离排序,则需要用mysql算距离,要拼接取字段的sql
+			$sql_view_field = 'select tmp.*, 6378.138 * 2 * ASIN(SQRT(POW(SIN(
+            (
+              '.$lat.' * PI() / 180 - latitude * PI() / 180
+            ) / 2
+          ), 2
+        ) + COS('.$lat.' * PI() / 180) * COS(latitude * PI() / 180) * POW(
+          SIN(
+            (
+              '.$lng.' * PI() / 180 - longitude * PI() / 180
+            ) / 2
+          ), 2
+        )
+      )
+    ) *1000 as distance from ';
+
+			$order = ' distance asc';
+		}
+
+		// 将sql 分开,便于后面拼接
+		$sql_skill = '(select ' . $s_field . ' from user_skill s left join users u on s.user_id = u.id where  s.status =1 ' . $s_where . ')';
+
+		$sql_union = ' union ';
+
+		$sql_demand = '(select ' . $d_field . ' from user_demand as d left join users u on d.user_id = u.id  where status=1 ' . $d_where . ' and end_time > ' . time(). ')';
+
+		$sql_order = ' order by ' . $order . ' limit ' . $offset . ',' . $page_size;
+
+
+		if($is_demand){
+			//单独获取需求
+			$sql = $sql_demand . $sql_order;
+		}elseif ($is_skill){
+			//单独获取技能列表
+			$sql = $sql_skill . $sql_order;
+		}elseif($is_distance){
+			// 按照距离排序
+			$sql = $sql_view_field . '(' . $sql_skill . $sql_union . $sql_demand . ') as tmp ' . $sql_order;
+		}else{
+			//通用
+			$sql = $sql_skill . $sql_union . $sql_demand . $sql_order;
+		}
 
 		return $this->query($sql);
 	}
