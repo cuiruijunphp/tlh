f36cd26d7a96c17679e688d4f31e0dc44a059998
cui <cuiruijunphp@163.com>
2019-12-27 23:10:21 +0800

    获取订单状态

--- Application/App/Controller/PayController.class.php
+++ Application/App/Controller/PayController.class.php
@@ -104,4 +104,49 @@ class PayController extends BaseController {
 			$this->result_return(null, 1, '更新状态失败');
 		}
 	}
+
+	/*
+	 * 主动获取支付订单状态
+	 */
+	public function get_order_result(){
+		$order_id = I('get.order_id');
+
+		$aliConfig = C('ALIPAY_CONFIG');
+
+		Vendor('Alipay.aop.AopClient');
+		Vendor('Alipay.aop.Request.AlipayTradeQueryRequest');
+
+		$aop = new \AopClient ();
+		$aop->gatewayUrl = $aliConfig['gatewayUrl'];
+		//appid
+		$aop->appId = $aliConfig['appId'];
+		//开发者私钥
+		$aop->rsaPrivateKey = $aliConfig['rsaPrivateKey'];
+		//支付宝公钥
+		$aop->alipayrsaPublicKey = $aliConfig['alipayPublicKey'];
+
+
+		$aop->apiVersion = '1.0';
+		$aop->signType = 'RSA2';
+		$aop->postCharset='UTF-8';
+		$aop->format='json';
+		$request = new \AlipayTradeQueryRequest();
+
+		$bizcontent = json_encode([
+			'out_trade_no' => $order_id,//此订单号为商户唯一订单号
+		]);
+
+		$request->setBizContent($bizcontent);
+		$result = $aop->execute ( $request);
+
+		$responseNode = str_replace(".", "_", $request->getApiMethodName()) . "_response";
+		$resultCode = $result->$responseNode->code;
+
+		if(!empty($resultCode) && $resultCode == 10000){
+			$result_update = json_decode(json_encode($result->alipay_trade_query_response), true);
+
+			$order_model = D('Order');
+			$res = $order_model->update_result($order_id, 'success', $result_update['total_amount'], strtotime($result_update['send_pay_date']), $result_update['trade_no']);
+		}
+	}
 }
