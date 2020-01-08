ce62e31fd2ff214a48313864f5093ab48ea55123
cui <cuiruijunphp@163.com>
2020-01-08 10:14:52 +0800

    订单如果更新过了，就不更新了

--- Application/Manage/Model/OrderModel.class.php
+++ Application/Manage/Model/OrderModel.class.php
@@ -102,9 +102,9 @@ class OrderModel extends CommonModel{
 			return '订单金额非法';
 		}
 
-//		if($order_info['payment_id'] && $order_info['pay_time']){
-//			return '订单已经更新过了';
-//		}
+		if($order_info['payment_id'] && $order_info['pay_time']){
+			return '订单已经更新过了';
+		}
 
 		$user_id = $order_info['user_id'];
 
