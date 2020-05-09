964cef3bb99356e5fcce97b25ab214ee2740df79
cuiruijunphp <cuiruijunphp@163.com>
2020-05-09 13:55:40 +0800

    用户隐私协议

---
+++ Application/App/Controller/AgreementController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Think\Controller;
+
+class AgreementController extends Controller {
+
+    public function user(){
+		$this->display();
+    }
+
+    public function privacy(){
+        $this->display();
+    }
+}
